07501fa523499adfbfea5d8f4ad07969a0bca37a
K1ngHandy <bolds-slicks0g@icloud.com>
2024-03-11 14:36:02 -0400

    demo

---
+++ demo.js
@@ -0,0 +1,30 @@
+// var something = 1;
+// var something = 2;
+// console.log(something); // 2
+
+// let something = 1;
+// let something = 2;
+// console.log(something); // SyntaxError: Identifier 'something' has already been declared
+
+// let something = 1;
+// something = 2;
+// console.log(something); // 2
+
+// const something = 1;
+// something = 2;
+// console.log(something); // TypeError: Assignment to constant variable.
+
+// sayHello(); // hello
+// function sayHello() {
+//     console.log('hello');
+// }
+
+// sayHello(); // ReferenceError: Cannot access 'sayHello' before initialization
+// const sayHello = () => {
+//     console.log('hello');
+// }
+
+const str = 'hello';
+str[1] = 'y';
+console.log(str); // hello
+
